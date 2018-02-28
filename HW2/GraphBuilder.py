ee009478613ca3e1d4fe1e3371c7f79c5707868d
xiaowang <wang.xiao1@husky.neu.edu>
2018-02-28 00:38:47 -0500

    HW2: refactor downloadHTML out

--- HW2/GraphBuilder.py
+++ HW2/GraphBuilder.py
@@ -1,47 +1,49 @@
-import os, re, collections
+import collections, requests, re
+from time import sleep
 from bs4 import BeautifulSoup
 
+WIKI = "https://en.wikipedia.org/wiki/"
+
 class GraphBuilder(object):
-    def __init__(self, dir):
-        self.dir = dir
-        self.docs = set(os.listdir(dir))
-        if ".DS_Store" in self.docs:
-            self.docs.remove(".DS_Store")
+    def __init__(self, inputFilename):
+        with open(inputFilename, 'r') as fin:
+            self.docs = set([line[30:].rstrip('\n') for line in fin.readlines()])
         self.graph = collections.defaultdict(list)
 
     def getLinks(self, docID):
-        with open(self.dir + '/' + docID, 'r') as file:
-            html = file.read().replace('\n', '')
-            body = BeautifulSoup(html, "html.parser").find(id="bodyContent")
-            links = set()
-            for href in body.find_all("a"):
-                link = href.get("href")
-                if link and re.match('/wiki/.*', link) is not None \
-                        and re.match('/wiki/Main_Page', link) is None \
-                        and re.match('/wiki/(.*)#(.*)', link) is None \
-                        and re.match('/wiki/(.*):(.*)', link) is None \
-                        and link[6:] in self.docs:
-                    links.add(link[6:])
-            return links
+        sleep(1)
+        URL = WIKI + docID
+        res = requests.get(URL)
+        body = BeautifulSoup(res.content, "html.parser").find(id="bodyContent")
+        links = set()
+        for href in body.find_all("a"):
+            link = href.get("href")
+            if link and re.match('/wiki/.*', link) is not None \
+                    and re.match('/wiki/Main_Page', link) is None \
+                    and re.match('/wiki/(.*)#(.*)', link) is None \
+                    and re.match('/wiki/(.*):(.*)', link) is None \
+                    and link[6:] in self.docs:
+                links.add(link[6:])
+        return links
 
     def build(self):
-        for doc in self.docs:
-            for link in self.getLinks(doc):
-                if link != doc:
-                    self.graph[link].append(doc)
+        for docID in self.docs:
+            for link in self.getLinks(docID):
+                if link != docID:
+                    self.graph[link].append(docID)
 
     def output(self, filename):
         with open(filename, 'w+') as file:
-            for doc, inlinks in self.graph.items():
+            for doc, inLinks in self.graph.items():
                 file.write(doc)
-                for inlink in inlinks:
-                    file.write(' ' + inlink)
+                for inLink in inLinks:
+                    file.write(' ' + inLink)
                 file.write('\n')
 
 
 if __name__ == "__main__":
-    inputDir = input("Enter the name of the directory containing wiki pages: ")
-    gb = GraphBuilder(inputDir)
+    inputFilename = input("Enter the filename containing wiki page links: ")
+    gb = GraphBuilder(inputFilename)
     gb.build()
     outputFile = input("Enter the output filename: ")
     gb.output(outputFile)
